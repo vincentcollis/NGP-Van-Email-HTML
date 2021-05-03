785e1e46af57490ec3487a29efb40c57779e3547
vincentcollis <vinsanity0050@gmail.com>
2021-05-03 11:24:01 -0400

    week 19 email

---
+++ index.js
@@ -0,0 +1,20 @@
+const str_one = "hello"
+const str_two = "good bye"
+
+const str_three = str_one + " " + str_two
+
+str_three
+
+
+
+
+
+
+
+const name = 'Skip';
+const phrase = ["My dog name is", name]
+
+const output = phrase.join(" ")//?
+
+
+=IF($B19='Flight Schedule'!$G21,'Flight Schedule'!$H21,IF($B19='Flight Schedule'!$G22,'Flight Schedule'!$H22,if($B19='Flight Schedule'!$G23,'Flight Schedule'!$H23,if($B19='Flight Schedule'!$G24,'Flight Schedule'!$H24,if($B19='Flight Schedule'!$G25,'Flight Schedule'!$H25,if($B19='Flight Schedule'!$G26,'Flight Schedule'!$H26,IF('Flight Schedule'!$G27,'Flight Schedule'!$H27,if($B19='Flight Schedule'!$G28,'Flight Schedule'!$H28,"-"))))))))
